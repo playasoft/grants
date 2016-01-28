75c8b3c35d2dae03564cd456107a64589837cc71
Rachel Fish <itsrachelfish@gmail.com>
2016-01-27 23:56:59 -0700

    Form validation for questions

--- laravel/app/Http/Controllers/QuestionController.php
+++ laravel/app/Http/Controllers/QuestionController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use App\Http\Requests\QuestionRequest;
+
+
 class QuestionController extends Controller
 {
     function listQuestions()
@@ -14,7 +17,7 @@ class QuestionController extends Controller
         return "[Insert list here]";
     }
 
-    function postQuestion()
+    function postQuestion(QuestionRequest $request)
     {
         return "Almost!";
     }
---
+++ laravel/app/Http/Requests/QuestionRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class QuestionRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return
+        [
+            'question' => 'required|min:3',
+            'type' => 'required|in:input,text,dropdown,boolean',
+            'status' => 'required|in:new,submitted,review,follow-up,accepted,rejected',
+            'role' => 'required|in:applicant,judge'
+        ];
+    }
+}
