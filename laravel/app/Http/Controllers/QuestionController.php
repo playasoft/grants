285623be6635f6ca679055082db760144ef1b044
Rachel Fish <itsrachelfish@gmail.com>
2016-01-30 15:48:12 -0700

    Now you can actually SAVE questions! *wow* :sunglasses:

--- laravel/app/Http/Controllers/QuestionController.php
+++ laravel/app/Http/Controllers/QuestionController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use App\Models\Question;
 use App\Http\Requests\QuestionRequest;
 
 
@@ -19,7 +20,14 @@ class QuestionController extends Controller
 
     function postQuestion(QuestionRequest $request)
     {
-        return "Almost!";
+        // Double check to make sure the current user is authorized to do this...
+        $this->authorize('create-question');
+
+        $input = $request->all();
+        $question = Question::create($input);
+
+        $request->session()->flash('success', 'Your question has been created.');
+        return redirect('/questions');
     }
 
     function createQuestionForm()
