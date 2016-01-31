86303fb161d8156a3567bf25c28c53b0612d2ea6
Rachel Fish <itsrachelfish@gmail.com>
2016-01-31 04:29:16 -0700

    Started about page

---
+++ laravel/resources/views/pages/about.blade.php
@@ -0,0 +1,11 @@
+@extends('app')
+
+@section('content')
+    <h1>About Weightlifter</h1>
+    <hr>
+
+    <p>
+        This website is a Question and Answer system where users can apply for art grants from Apogaea.
+        Administrators create questions and assign judges, users fill out applications to be reviewed by the Apogaea team.
+    </p>
+@endsection
