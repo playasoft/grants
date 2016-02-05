4ac049f42e17f1698aada77bf86a78401d455fb7
Rachel Fish <itsrachelfish@gmail.com>
2016-02-04 22:39:46 -0700

    Now you can save answers!

--- laravel/app/Http/Controllers/AnswerController.php
+++ laravel/app/Http/Controllers/AnswerController.php
@@ -12,13 +12,32 @@ use App\Models\Question;
 use App\Models\Answer;
 
 use App\Http\Requests\AnswerRequest;
-
+use Illuminate\Support\Facades\Auth;
 
 class AnswerController extends Controller
 {
     public function createAnswer(AnswerRequest $request)
     {
-        return "// todo";
+        // Check if current user created this application
+        $input = $request->all();
+
+        $application = Application::find($input['application_id']);
+        $question = Question::find($input['question_id']);
+
+        if($application->user->id != Auth::user()->id)
+        {
+            $request->session()->flash('error', 'Only the person who created an application may answer questions for it.');
+            return redirect('/login');
+        }
+
+        $answer = new Answer;
+        $answer->application_id = $application->id;
+        $answer->question_id = $question->id;
+        $answer->answer = $input['answer'];
+        $answer->save();
+
+        $request->session()->flash('success', 'Your answer has been saved.');
+        return redirect('/applications/' . $application->id);
     }
 
     public function updateAnswer(AnswerRequest $request, Answer $answer)
