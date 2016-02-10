5bb8858c7b855d51f77837ac8dd1cc08aedb210b
Rachel Fish <itsrachelfish@gmail.com>
2016-02-10 04:54:10 -0700

    Only make AJAX request when values change

--- laravel/resources/js/ajax.js
+++ laravel/resources/js/ajax.js
@@ -3,10 +3,19 @@ var timeout = false;
 
 $(document).ready(function()
 {
+    // On load, populate current data values
+    $('form.ajax').find('input, select, textarea').each(function()
+    {
+        var value = $(this).value();
+        $(this).data('saved', value);
+    });
+
     $('form.ajax').find('input, select, option, textarea').on('keydown click change', function(event)
     {
         var form = $(this).parents('form.ajax');
         var status = $(this).parents('.form-group').find('.status');
+        var value = $(this).value();
+        var input = $(this);
         
         if(timeout)
         {
@@ -18,6 +27,20 @@ $(document).ready(function()
         
         timeout = setTimeout(function()
         {
+            console.log(value, input.data('saved-value'));
+
+            // If no changes have been made, abort
+            if(input.data('saved') == value)
+            {
+                // Fade the saved text out after 1 second
+                setTimeout(function()
+                {
+                    status.addClass('complete');
+                }, 1000);
+
+                return;
+            }
+
             status.text('Saving...');
 
             // Get form data
@@ -28,6 +51,8 @@ $(document).ready(function()
             {
                 status.text('Saved!');
                 status.attr('class', 'status success');
+
+                input.data('saved', value);
 
                 // Fade the saved text out after 1 second
                 setTimeout(function()
