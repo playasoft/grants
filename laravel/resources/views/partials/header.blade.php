1b013dce16ef71cb3259fd3b3303156dc3b7d4d3
Marc Jones <marcj303@gmail.com>
2016-02-16 16:08:03 -0700

    nav: Add Apogaea logo
    Signed-off-by: Marc Jones <marcj303@gmail.com>

--- laravel/resources/views/partials/header.blade.php
+++ laravel/resources/views/partials/header.blade.php
@@ -5,20 +5,21 @@
         </div>
 
         <div class="navbar-header">
+            <a class="pull-left" href="/"><img style="padding:5px" src="http://apogaea.com/wp-content/themes/aporust/javascript/particle.png"></a>
             <a class="navbar-brand" href="/">Weightlifter</a>
         </div>
 
-        <div class="collapse navbar-collapse"> 
+        <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
                 <li><a href="/about">About</a></li>
-                
+
                 @if(Auth::check() && in_array(Auth::user()->role, ['admin', 'judge', 'observer']))
                     <li><a href="/users">Users</a></li>
                     <li><a href="/questions">Questions</a></li>
                     <li><a href="/applications">Applications</a></li>
                 @endif
             </ul>
-            
+
             <ul class="nav navbar-nav navbar-right">
                 @if(Auth::check())
                     <li><a href="/logout">Logout</a></li>
