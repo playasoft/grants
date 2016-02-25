7cbc82df12d3a160b4e169648c6ebfcff6f4f7c2
Marc Jones <marcj303@gmail.com>
2016-02-25 14:16:01 -0700

    Add user and admin profile views
    Any user may update thier own profile. Judges and observers may view all
    the user profiles. The admin may update any user data.
    
    Signed-off-by: Marc Jones <marcj303@gmail.com>

--- laravel/resources/views/pages/users/view.blade.php
+++ laravel/resources/views/pages/users/view.blade.php
@@ -1,8 +1,12 @@
 @extends('app')
 
 @section('content')
-    <h1>Viewing User: {{ $user->name }}</h1>
+    <h1>Viewing User:  <b>{{ $user->name }} </b> </h1>
     <hr>
+
+    @if(in_array(Auth::user()->role, ['admin']))
+        <a href="/users/{{ $user->id }}/edit">(edit)</a>
+    @endif
 
     <div class="profile">
         <div class="row">
@@ -14,6 +18,12 @@
             <div class="col-sm-2 title">Email</div>
             <div class="col-sm-10 value">{{ $user->email }}</div>
         </div>
+
+        <div class="row">
+            <div class="col-sm-2 title">Role</div>
+            <div class="col-sm-10 value">{{ $user->role }}</div>
+        </div>
+
         <div class="row">
             <div class="col-sm-2 title">User_ID</div>
             <div class="col-sm-10 value">{{ $user->id }}</div>
@@ -24,7 +34,7 @@
             <div class="col-sm-10 value">{{ $user->created_at->format('Y-m-d') }}</div>
         </div>
 
-        <h3>Additional information</h3>
+        <h3>Profile</h3>
 
          <div class="row">
             <div class="col-sm-2 title">Burner Name</div>
@@ -45,4 +55,5 @@
             <div class="col-sm-2 title">Phone</div>
             <div class="col-sm-10 value">{{ $user->data->phone or 'Not Provided' }}</div>
         </div>
+    </div>
 @endsection
--- laravel/resources/views/partials/header.blade.php
+++ laravel/resources/views/partials/header.blade.php
@@ -23,7 +23,7 @@
 
             <ul class="nav navbar-nav navbar-right">
                 @if(Auth::check())
-                    <li><a href="/users/{{ Auth::user()->id }}">Profile</a></li>
+                    <li><a href="/users/profile">Profile</a></li>
                     <li><a href="/logout">Logout</a></li>
                 @else
                     <li><a href="/login">Login</a></li>
