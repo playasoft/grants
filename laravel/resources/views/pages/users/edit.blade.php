e21ffe443ee7f9a2384857086f9a3306e0b6f9d3
Rachel Fish <itsrachelfish@gmail.com>
2016-02-26 03:27:35 -0700

    Fixed typo in user profile page

--- laravel/resources/views/pages/users/edit.blade.php
+++ laravel/resources/views/pages/users/edit.blade.php
@@ -45,8 +45,8 @@
 
         @include('partials/form/text', ['name' => 'burner_name', 'label' => 'Burner Name', 'placeholder' => 'Your name on the Playa', 'value' => (is_null($user->data)) ? '' : $user->data->burner_name])
         @include('partials/form/text', ['name' => 'real_name', 'label' => 'Real Name', 'placeholder' => 'Your name in the Default World', 'value' => (is_null($user->data)) ? '' : $user->data->real_name])
-        @include('partials/form/textarea', ['name' => 'address', 'label' => 'Address', 'placeholder' => 'Your name mailing address', 'value' => (is_null($user->data)) ? '' : $user->data->address])
-        @include('partials/form/text', ['name' => 'phone', 'label' => 'Phone Number', 'placeholder' => 'Your name 10 digit phone number', 'value' => (is_null($user->data)) ? '' : $user->data->phone])
+        @include('partials/form/textarea', ['name' => 'address', 'label' => 'Address', 'placeholder' => 'Your mailing address', 'value' => (is_null($user->data)) ? '' : $user->data->address])
+        @include('partials/form/text', ['name' => 'phone', 'label' => 'Phone Number', 'placeholder' => 'Your 10 digit phone number', 'value' => (is_null($user->data)) ? '' : $user->data->phone])
 
         <button type="submit" class="btn btn-success">Save Changes</button>
         <a href="/profile" class="btn btn-primary">Cancel</a>
