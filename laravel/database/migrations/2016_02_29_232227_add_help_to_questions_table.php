af30096312d0c8132a313c8c6c62721214ae3e4b
Marc Jones <marcj303@gmail.com>
2016-02-29 17:54:44 -0700

    Add question help field migration
    Signed-off-by: Marc Jones <marcj303@gmail.com>

---
+++ laravel/database/migrations/2016_02_29_232227_add_help_to_questions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddHelpToQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->text('help');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->dropColumn('help');
+        });
+    }
+}
