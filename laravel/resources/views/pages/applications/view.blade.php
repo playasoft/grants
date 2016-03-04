0d5cc489c24e3a73ed77332baec002a0aa385d9f
Marc Jones <marcj303@gmail.com>
2016-03-04 14:09:37 -0700

    Update answer view for document upload
    Support uploading documents for a 'file' question and general
    document uploading. If there is a file upload question, it displays
    any files associated with it and has an upload box. In addition,
    there is a general file upload section at the bottom.
    
    This fix also changes the upload interface from ajax autsave to a
    submit button. The ajax saving is intuitive for the question text
    fields, but not the file uploads. The page didn't update with the
    file uploaded and it was easy to upload a file multiple times by
    changing focus on the upload box.
    
    Signed-off-by: Marc Jones <marcj303@gmail.com>

--- laravel/resources/views/pages/applications/view.blade.php
+++ laravel/resources/views/pages/applications/view.blade.php
@@ -13,7 +13,7 @@
             'placeholder' => "Blinky Sparkle Pony",
             'value' => $application->name
         ])
-        
+
         @include('partials/form/textarea',
         [
             'name' => 'description',
@@ -42,44 +42,51 @@
         }
 
         ?>
-    
-        {!! Form::open(['url' => $action, 'files' => true, 'class' => 'ajax']) !!}
-            <input type="hidden" name="application_id" value="{{ $application->id }}">
-            <input type="hidden" name="question_id" value="{{ $question->id }}">
+        @if($question->type != 'file')
+            {!! Form::open(['url' => $action, 'class' => 'ajax']) !!}
+                <input type="hidden" name="application_id" value="{{ $application->id }}">
+                <input type="hidden" name="question_id" value="{{ $question->id }}">
 
-            <div class="form-group">
-                <label class="control-label" for="{{ $question->id }}-answer">{{ $question->question }}</label>
-                <span class="pull-right"><span class="status"></span></span>
+                <div class="form-group">
+                    <label class="control-label" for="{{ $question->id }}-answer">{{ $question->question }}</label>
+                    <span class="pull-right"><span class="status"></span></span>
+                    @if($question->type == 'input')
+                        <input type="text" name="answer" class="form-control" id="{{ $question->id }}-answer" value="{{ $answer }}">
+                    @elseif($question->type == 'text')
+                        <textarea name="answer" class="form-control" id="{{ $question->id }}-answer">{{ $answer }}</textarea>
+                    @elseif($question->type == 'boolean')
+                        <div class="radio">
+                            <label>
+                                <input type="radio" name="answer" value="yes" id="{{ $question->id }}-answer" {{ ($answer == 'yes') ? 'checked' : '' }}> Yes
+                            </label>
+                            <br>
+                            <label>
+                                <input type="radio" name="answer" value="no" id="{{ $question->id }}-answer" {{ ($answer == 'no') ? 'checked' : '' }}> No
+                            </label>
+                        </div>
+                    @elseif($question->type == 'dropdown')
+                        <select class="form-control" name="answer" id="{{ $question->id }}-answer">
+                            <option value="">----</option>
 
-                @if($question->type == 'input')
-                    <input type="text" name="answer" class="form-control" id="{{ $question->id }}-answer" value="{{ $answer }}">
-                @elseif($question->type == 'text')
-                    <textarea name="answer" class="form-control" id="{{ $question->id }}-answer">{{ $answer }}</textarea>
-                @elseif($question->type == 'boolean')
-                    <div class="radio">
-                        <label>
-                            <input type="radio" name="answer" value="yes" id="{{ $question->id }}-answer" {{ ($answer == 'yes') ? 'checked' : '' }}> Yes
-                        </label>
-                        <br>
-                        <label>
-                            <input type="radio" name="answer" value="no" id="{{ $question->id }}-answer" {{ ($answer == 'no') ? 'checked' : '' }}> No
-                        </label>
-                    </div>
-                @elseif($question->type == 'dropdown')
-                    <select class="form-control" name="answer" id="{{ $question->id }}-answer">
-                        <option value="">----</option>
-                        
-                        @foreach($question->dropdown() as $value => $option)
-                            <option value="{{ $value }}" {{ ($answer == $value) ? 'selected' : '' }}>{{ $option }}</option>
-                        @endforeach
-                    </select>
-                @elseif($question->type == 'file')
-                    <input type="hidden" name="answer" value="1">
+                            @foreach($question->dropdown() as $value => $option)
+                                <option value="{{ $value }}" {{ ($answer == $value) ? 'selected' : '' }}>{{ $option }}</option>
+                            @endforeach
+                        </select>
+                    @endif
+                    <label class="" for="{{ $question->id }}-answer">{!! nl2br(e($question->help)) !!}</label>
+                </div>
+            {!! Form::close() !!}
+        @else
+            {!! Form::open(['url' => $action, 'files' => true,]) !!}
+                <input type="hidden" name="application_id" value="{{ $application->id }}">
+                <input type="hidden" name="question_id" value="{{ $question->id }}">
+                <input type="hidden" name="answer" value="1">
 
-                    @if($answer && $answers[$question->id]->documents->count())
+                <div class="form-group">
+                    <label class="control-label" for="{{ $question->id }}-answer">{{ $question->question }}</label>
+                    <span class="pull-right"><span class="status"></span></span>
+                    @if($answers[$question->id]->documents->count())
                         <div>
-                            <b>Uploaded Files</b>
-
                             <ul class="documents">
                                 @foreach($answers[$question->id]->documents as $document)
                                     <li>
@@ -89,22 +96,46 @@
                                 @endforeach
                             </ul>
                         </div>
-
-                        <b>Upload Another File</b>
                     @endif
-
                     <div>
                         <span class="btn btn-primary btn-file">
-                            <input type="file" name="document" id="{{ $question->id }}-answer">
+                            <input type="file" name="document">
+                            <button type="submit" class="btn btn-success">Upload</button>
                         </span>
                     </div>
-                @endif
-                <label class="" for="{{ $question->id }}-answer">{!! nl2br(e($question->help)) !!}</label>
-            </div>
-        {!! Form::close() !!}
-
+                    <label class="" for="{{ $question->id }}-answer">{!! nl2br(e($question->help)) !!}</label>
+                </div>
+            {!! Form::close() !!}
+        @endif
         <hr>
     @endforeach
+
+    <h2>Application Files</h2>
+    <p>
+        Upload any additional application files; drawings, plans, photos, past projects, etc.
+    </p>
+
+    {!! Form::open(['url' => '/documents/' . $application->id . '/add', 'files' => true,]) !!}
+        @if($application->documents->count())
+            <div>
+                <ul class="documents">
+                    @foreach($application->documents as $document)
+                        <li>
+                            <a class="document" href="/files/user/{{ $document->file }}">{{ $document->name }}</a>
+                            <a href="/documents/{{ $document->id }}/delete" class="btn btn-danger">Delete</a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <div>
+            <span class="btn btn-primary btn-file">
+                <input type="file" name="document">
+                <button type="submit" class="btn btn-success">Upload</button>
+            </span>
+        </div>
+    {!! Form::close() !!}
+    <hr>
 
     <a href="/applications/{{ $application->id }}/review" class="btn btn-success">Submit Application</a>
 @endsection
