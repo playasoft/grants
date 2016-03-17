a814e16b4fbf656b5cfce254f71d1b943405f997
Marc Jones <marcj303@gmail.com>
2016-03-16 21:39:37 -0600

    Add contact info to the footer
    Add the grants@apogaea email to the footer, so that it is on every page.
    f someone has a problem the may not look on the home or about pages.
    
    Signed-off-by: Marc Jones <marcj303@gmail.com>

--- laravel/resources/views/partials/footer.blade.php
+++ laravel/resources/views/partials/footer.blade.php
@@ -1,6 +1,7 @@
 <div class="footer container-fluid">
     <hr>
 
+    If you have problems or questions, please contact <a href="mailto:grants@apogaea.com">grants@apogaea.com</a><br>
     This project is 100% Free Open Source Software, available via <a href="https://github.com/itsrachelfish/weightlifter">GitHub</a>.<br>
     Made with love by <a href="https://github.com/itsrachelfish/weightlifter/graphs/contributors">nerds</a> <span class="heart glyphicon glyphicon-heart"></span>
 </div>
