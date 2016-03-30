e525f8df16619223d4de339cbc1b579671a91b88
Rachel Fish <itsrachelfish@gmail.com>
2016-03-29 23:33:18 -0600

    Converted old user welcome listener to generic handler

--- laravel/app/Listeners/SendUserMessage.php
+++ laravel/app/Listeners/SendUserMessage.php
@@ -10,6 +10,13 @@ use App\Models\User;
 
 class SendUserMessage
 {
+    private $handlers =
+    [
+        "App\Events\UserRegistered" => "userRegistered",
+        "App\Events\ApplicationSubmitted" => "applicationSubmitted",
+        "App\Events\ApplicationChanged" => "applicationChanged",
+    ];
+
     /**
      * Create the event listener.
      *
@@ -27,6 +34,31 @@ class SendUserMessage
      * @return void
      */
     public function handle($event)
+    {
+        $class = get_class($event);
+
+        if(isset($this->handlers[$class]))
+        {
+            call_user_func(array($this, $this->handlers[$class]), $event);
+        }
+    }
+
+    private function userRegistered($event)
+    {
+        $user = $event->user;
+
+        Mail::send('emails/user-welcome', compact('user'), function ($message) use ($user)
+        {
+            $message->to($user->email, $user->name)->subject('Welcome to Weightlifter!');
+        });
+    }
+
+    private function applicationSubmitted($event)
+    {
+        // todo
+    }
+
+    private function applicationChanged($event)
     {
         // todo
     }
