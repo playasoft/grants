77c487163d8fe5466a3784431a9e5a3796a58e7f
Marc Jones <marcj303@gmail.com>
2016-12-12 22:40:20 -0700

    Scroll and stripe application and score tables
    Make tables easier to read.
    
    Signed-off-by: Marc Jones <marcj303@gmail.com>

--- laravel/resources/views/pages/applications/list.blade.php
+++ laravel/resources/views/pages/applications/list.blade.php
@@ -9,44 +9,45 @@
     @foreach($rounds as $round)
         <hr>
         <h2> {{ $round->name }} </h2>
+        <div class="scrollable">
+            <table class="table table-hover table-striped">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Applicant</th>
+                        <th>Status</th>
+                        <th>Judge Status</th>
+                        <th>Score</th>
+                        <th>Created</th>
+                        <th>Last Modified</th>
+                    </tr>
+                </thead>
 
-        <table class="table table-hover">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Applicant</th>
-                    <th>Status</th>
-                    <th>Judge Status</th>
-                    <th>Score</th>
-                    <th>Created</th>
-                    <th>Last Modified</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                @foreach($applications as $application)
-                    @if($application->round_id == $round->id)
-                        <tr>
-                            <td>
-                                @if($application->status == 'new')
-                                    <a href="/applications/{{ $application->id }}">{{ $application->name }}</a>
-                                @else
-                                    <a href="/applications/{{ $application->id }}/review">{{ $application->name }}</a>
-                                @endif
-                            </td>
-                            <td>
-                                <a href="/users/{{ $application->user->id }}">{{ $application->user->name }}</a>
-                            </td>
-                            <td>{{ $application->status }}</td>
-                            <td>{{ $application->judge_status }}</td>
-                            <td>{{ $application->objective_score }} / {{ $application->subjective_score }} / {{ $application->total_score }}</td>
-                            <td>{{ $application->created_at->format('Y-m-d H:i:s e') }}</td>
-                            <td>{{ $application->updated_at->format('Y-m-d H:i:s e') }}</td>
-                        </tr>
-                    @endif
-                @endforeach
-            </tbody>
-        </table>
-        <br>
+                <tbody>
+                    @foreach($applications as $application)
+                        @if($application->round_id == $round->id)
+                            <tr>
+                                <td>
+                                    @if($application->status == 'new')
+                                        <a href="/applications/{{ $application->id }}">{{ $application->name }}</a>
+                                    @else
+                                        <a href="/applications/{{ $application->id }}/review">{{ $application->name }}</a>
+                                    @endif
+                                </td>
+                                <td>
+                                    <a href="/users/{{ $application->user->id }}">{{ $application->user->name }}</a>
+                                </td>
+                                <td>{{ $application->status }}</td>
+                                <td>{{ $application->judge_status }}</td>
+                                <td>{{ $application->objective_score }} / {{ $application->subjective_score }} / {{ $application->total_score }}</td>
+                                <td>{{ $application->created_at->format('Y-m-d H:i:s e') }}</td>
+                                <td>{{ $application->updated_at->format('Y-m-d H:i:s e') }}</td>
+                            </tr>
+                        @endif
+                    @endforeach
+                </tbody>
+            </table>
+            <br>
+        </div>
     @endforeach
 @endsection
--- laravel/resources/views/pages/scores/list.blade.php
+++ laravel/resources/views/pages/scores/list.blade.php
@@ -7,30 +7,32 @@
         @foreach($rounds as $round)
             <hr>
             <h2> {{ $round->name }} </h2>
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                    <th>Name</th>
-                        @foreach($criteria as $criterion)
-                            <th title="{{ $criterion->question }}">C {{ $criterion->id }}</th>
-                        @endforeach
-                    </tr>
-                </thead>
-
-                <tbody>
-                    @foreach($applications as $application)
-                        @if($application->round_id == $round->id)
-                            <tr>
-                            <td><a href="/scores/{{ $application->id }}">{{ $application->name }}</a></td>
+            <div class="scrollable">
+                <table class="table table-hover table-striped">
+                    <thead>
+                        <tr>
+                        <th>Name</th>
                             @foreach($criteria as $criterion)
-                                <td>{{ round($appScores[$application->id][$criterion->id], 3) }}</td>
+                                <th title="{{ $criterion->question }}">C {{ $criterion->id }}</th>
                             @endforeach
-                            </tr>
-                        @endif
-                    @endforeach
-                </tbody>
-            </table>
-            <br>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        @foreach($applications as $application)
+                            @if($application->round_id == $round->id)
+                                <tr>
+                                <td><a href="/scores/{{ $application->id }}">{{ $application->name }}</a></td>
+                                @foreach($criteria as $criterion)
+                                    <td>{{ round($appScores[$application->id][$criterion->id], 3) }}</td>
+                                @endforeach
+                                </tr>
+                            @endif
+                        @endforeach
+                    </tbody>
+                </table>
+                <br>
+            </div>
         @endforeach
     @endcan
 @endsection
