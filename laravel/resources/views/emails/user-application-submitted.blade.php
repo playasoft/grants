caefcc6da3561522a69b34b83660cf33a05462bc
Marc Jones <marcj303@gmail.com>
2017-12-18 21:56:46 -0700

    Update email message
    Update email to add more infrmation about withdrawing, Q&A,
    and contacting grants@apogaea.com for questions.
    
    Signed-off-by: Marc Jones <marcj303@gmail.com>

--- laravel/resources/views/emails/user-application-submitted.blade.php
+++ laravel/resources/views/emails/user-application-submitted.blade.php
@@ -2,11 +2,15 @@
 
 <p>
     This email is to confirm that your project "<b>{{ $application->name }}</b>" has been submitted for final review by our judges.
+    You may withdraw (and re-submit) your application up until the final deadline.
     Submitting this application does not guarantee that you will recieve funding.
-    You may recieve additional emails if additional questions or feedback are requested.
+    You may receive additional emails if additional questions are asked in the Q&A period following the grant deadline.
     You will be notified when your application is either approved or denied.
 </p>
 
 <p>
     To review your submitted application, log into your account on <a href="{{ env('SITE_URL') }} ">{{ env('SITE_NAME') }}</a>.
+</p>
+<p>
+    If you have any questions or need assistance with your application, please email <a href="mailto:grants@apogaea.com" target="_blank">grants@apogaea.com</a>.
 </p>
