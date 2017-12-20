401fba53cdedc27425621799cd654d00496a660c
Rachel Fish <itsrachelfish@gmail.com>
2017-12-20 02:18:25 -0700

    Updated user profile page
    - Changed edit button to use bootstrap classes
    - Display message when user has no applications
    - Add round name to user list of applications

--- laravel/resources/views/pages/users/view.blade.php
+++ laravel/resources/views/pages/users/view.blade.php
@@ -5,7 +5,9 @@
     <hr>
 
     @if(in_array(Auth::user()->role, ['admin']))
-        <a href="/users/{{ $user->id }}/edit">(edit)</a>
+        <div class="pull-right">
+            <a href="/users/{{ $user->id }}/edit" class="btn btn-success">Edit Profile</a>
+        </div>
     @endif
 
     <div class="profile">
@@ -56,38 +58,46 @@
             <div class="col-sm-10 value">{{ $user->data->phone or 'Not Provided' }}</div>
         </div>
     </div>
-    <div>
-    <h2>Applications</h2>
-    <table class="table table-hover">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Status</th>
-                <th>Judge Status</th>
-                <th>Score</th>
-                <th>Created</th>
-                <th>Last Modified</th>
-            </tr>
-        </thead>
 
-        <tbody>
-            @foreach($user->applications as $application)
+    <h2>Applications</h2>
+
+    @if(count($user->applications))
+        <table class="table table-hover">
+            <thead>
                 <tr>
-                    <td>
-                        @if($application->status == 'new')
-                            <a href="/applications/{{ $application->id }}">{{ $application->name }}</a>
-                        @else
-                            <a href="/applications/{{ $application->id }}/review">{{ $application->name }}</a>
-                        @endif
-                    </td>
-                    <td>{{ $application->status }}</td>
-                    <td>{{ $application->judge_status }}</td>
-                    <td>{{ $application->objective_score }} / {{ $application->subjective_score }} / {{ $application->total_score }}</td>
-                    <td>{{ $application->created_at->format('Y-m-d H:i:s e') }}</td>
-                    <td>{{ $application->updated_at->format('Y-m-d H:i:s e') }}</td>
+                    <th>Round</th>
+                    <th>Name</th>
+                    <th>Status</th>
+                    <th>Judge Status</th>
+                    <th>Score</th>
+                    <th>Created</th>
+                    <th>Last Modified</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-    </div>
+            </thead>
+
+            <tbody>
+                @foreach($user->applications as $application)
+                    <tr>
+                        <td>
+                            {{ $application->round->name }}
+                        </td>
+                        <td>
+                            @if($application->status == 'new')
+                                <a href="/applications/{{ $application->id }}">{{ $application->name }}</a>
+                            @else
+                                <a href="/applications/{{ $application->id }}/review">{{ $application->name }}</a>
+                            @endif
+                        </td>
+                        <td>{{ $application->status }}</td>
+                        <td>{{ $application->judge_status }}</td>
+                        <td>{{ $application->objective_score }} / {{ $application->subjective_score }} / {{ $application->total_score }}</td>
+                        <td>{{ $application->created_at->format('Y-m-d H:i:s e') }}</td>
+                        <td>{{ $application->updated_at->format('Y-m-d H:i:s e') }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @else
+        No applications submitted :(
+    @endif
 @endsection
