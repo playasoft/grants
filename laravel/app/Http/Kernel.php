bba278992d8ddb40c675c20b3c085723957775d5
tom-almquist <thomas.c.almquist@gmail.com>
2018-03-21 20:33:54 -0500

    fixed implicit binding in UserController

--- laravel/app/Http/Kernel.php
+++ laravel/app/Http/Kernel.php
@@ -32,11 +32,13 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
         'api' =>
         [
             'throttle:60,1',
+            'bindings',
         ],
 
         'auth' =>
