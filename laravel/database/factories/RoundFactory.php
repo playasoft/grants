42b5c985d3556bd3fc6e98ba422506fcabb02c29
tom-almquist <thomas.c.almquist@gmail.com>
2018-03-22 19:06:25 -0500

    Generated seeders and factory templates

---
+++ laravel/database/factories/RoundFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Round::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ laravel/database/seeds/RoundsTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RoundsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
