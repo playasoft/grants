cfe8f0e93f072bf2c015adaa7f71c92a793687d7
Rachel Fish <itsrachelfish@gmail.com>
2024-02-05 01:37:55 -0700

    Fixed formatting in signature controller, refactored SigningStatus

--- laravel/app/Http/Controllers/SignatureController.php
+++ laravel/app/Http/Controllers/SignatureController.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -21,182 +19,154 @@ use App\Models\Round;
 
 use App\Http\Requests\ApplicationRequest;
 use App\Misc\Helper;
-use Illuminate\Support\Facades\Validator; 
-
+use Illuminate\Support\Facades\Validator;
 
 class SignatureController extends Controller
-
 {
+    //Read env file to grab variables
+    public $Auth_token;
+    public $template_ID;
+    public $Doc_Seal_API;
 
-  //Read env file to grab variables 
-  public $Auth_token;
-  public $template_ID;
-  public $Doc_Seal_API;
-
-  public function __construct()
-  {
-      $this->Auth_token = "X-Auth-Token:" . env('DOCSEAL_TOKEN');
-      $this->template_ID = env('DOCSEAL_TEMPLATE');
-      $this->Doc_Seal_API = env('DOCSEAL_API_URL');
-  }
-  
-public function setStream($action, $data)
-{
-
-  //Set URL
-  $url = $this->Doc_Seal_API;
-
-  //Set Method for action
-  if ($action === "Status") 
-  {
-    //Set method to GET, and then append data passed (which should be the contract ID) to url
-      $method ="GET";
-      $url = $url."/$data";
-  }
-  elseif ($action === "Create")
+    public function __construct()
     {
-      $method="POST";
-    /*  $contextOptions = [
-        'http' => [
-          'content' => json_encode($data),
-        ],
-      ];
-    */
-    
+        $this->Auth_token = "X-Auth-Token:" . env('DOCSEAL_TOKEN');
+        $this->template_ID = env('DOCSEAL_TEMPLATE');
+        $this->Doc_Seal_API = env('DOCSEAL_API_URL');
     }
-        
-    $contextOptions = [
-      'http' => [
-          'method' => $method,
-          'header' =>  "Content-type: application/json\r\n". $this->Auth_token,
-          'content' => json_encode($data),
-      ],
-      'ssl' => [
-          'verify_peer' => true,
-          'verify_peer_name' => true,
-          'allow_self_signed' => false,
-      ],
-  ];
-  
-  $stream_array = [
-    'url' => $url,
-    'contextOptions' => stream_context_create($contextOptions),
-  ];
 
-  return $stream_array;
-  
+    public function setStream($action, $data)
+    {
+        //Set URL
+        $url = $this->Doc_Seal_API;
 
-}
+        //Set Method for action
+        if($action === "Status")
+        {
+            //Set method to GET, and then append data passed (which should be the contract ID) to url
+            $method = "GET";
+            $url = $url."/$data";
+        }
+        elseif($action === "Create")
+        {
+            $method = "POST";
+        }
+
+        $contextOptions = [
+            'http' => [
+                'method' => $method,
+                'header' =>  "Content-type: application/json\r\n". $this->Auth_token,
+                'content' => json_encode($data),
+            ],
+            'ssl' => [
+                'verify_peer' => true,
+                'verify_peer_name' => true,
+                'allow_self_signed' => false,
+            ],
+        ];
+
+        $stream_array = [
+            'url' => $url,
+            'contextOptions' => stream_context_create($contextOptions),
+        ];
+
+        return $stream_array;
+    }
 
     public function createSigning(Application $application, User $user)
     {
+        //Find User ID Data
+        $user_data = UserData::where ('user_id', $user->id)->first();
 
-
-      //Find User ID Data
-      $user_data = UserData::where ('user_id', $user->id)->first();
-
-      // Set payload to create the signature
-      $signature_payload = [
-        "template_id" => $this->template_ID,
-        "submission" => [
-            [
-                "submitters" => [
+        // Set payload to create the signature
+        $signature_payload = [
+            "template_id" => $this->template_ID,
+            "submission" => [
+                [
+                    "submitters" =>
                     [
-                        "name" => $user_data->real_name,
-                        "email" => $user_data->email,
-                        "phone" => $user_data->phone,
-                        "values" => [],
-                        "application_key" => "string",
-                        "fields" => [
-                            ["name" => "Artist", "default_value" => $user_data->real_name],
-                            ["name" => "Amount", "default_value" => $application->approved_budget],
-                            ["name" => "art_name", "default_value" => $application->name],
-                            ["name" => "Description", "default_value" => $application->description]
+                        [
+                            "name" => $user_data->real_name,
+                            "email" => $user_data->email,
+                            "phone" => $user_data->phone,
+                            "values" => [],
+                            "application_key" => "string",
+                            "fields" => [
+                                ["name" => "Artist", "default_value" => $user_data->real_name],
+                                ["name" => "Amount", "default_value" => $application->approved_budget],
+                                ["name" => "art_name", "default_value" => $application->name],
+                                ["name" => "Description", "default_value" => $application->description]
+                            ]
                         ]
                     ]
                 ]
             ]
-        ]
-      ];
+        ];
 
-      // Get stream array
-      $stream_array = $this->setStream("Create", $signature_payload);
-      
-      //Open file and get response
-      $response = file_get_contents($stream_array['url'], false, $stream_array['contextOptions']);
+        // Get stream array
+        $stream_array = $this->setStream("Create", $signature_payload);
 
+        //Open file and get response
+        $response = file_get_contents($stream_array['url'], false, $stream_array['contextOptions']);
 
-       if ($response === false) {
-          return array("error" => error_get_last());
-      }
-      else {
-        $responseData = json_decode($response, true);
-        
-        if (array_key_exists('error',$responseData)){
-          return $responseData;
+        if($response === false)
+        {
+            return array("error" => error_get_last());
         }
-        else {
-        #Create a new signature and set values; slug is appeneded to the Contract URL to create the signature link. 
-         $newContract = new Signature();
-         $newContract->contractID = $responseData[0]["submission_id"];
-         $newContract->slug = $responseData[0]["slug"];
-         $newContract->status = "sent";
-         $newContract->created_at = strtotime($responseData[0]["sent_at"]);
-         $newContract->save();
-         return $newContract;
-       }
+        else
+        {
+            $responseData = json_decode($response, true);
 
-      }
-
-
-          
+            if(array_key_exists('error',$responseData))
+            {
+                return $responseData;
+            }
+            else
+            {
+                #Create a new signature and set values; slug is appeneded to the Contract URL to create the signature link.
+                $newContract = new Signature();
+                $newContract->contractID = $responseData[0]["submission_id"];
+                $newContract->slug = $responseData[0]["slug"];
+                $newContract->status = "sent";
+                $newContract->created_at = strtotime($responseData[0]["sent_at"]);
+                $newContract->save();
+                return $newContract;
+            }
+        }
     }
 
-      
+    public function SigningStatus(Signature $signature)
+    {
+        // Set stream options, passing Status and the Contract ID
+        $stream_array = $this->SetStream("Status", $signature->contractID);
+        $response = file_get_contents($stream_array['url'], false, $stream_array['contextOptions']);
 
-                      
-    
+        // Default to error, if there is a response then the output will be overwritten
+        $status = "error";
 
-        
-    
+        if($response)
+        {
+            $responseData = json_decode($response, true);
 
+            if(isset($responseData["submitters"][0]["sent_at"])) {
+                $signature->created_at = strtotime($responseData["submitters"][0]["sent_at"]);
+                $status = "sent";
+            }
 
-  
+            if(isset($responseData["submitters"][0]["opened_at"])) {
+                $signature->updated_at = strtotime($responseData["submitters"][0]["opened_at"]);
+                $status = "opened";
+            }
 
-    public function SigningStatus(Signature $signature) {
-
-      // Set stream options, passing Status and the Contract ID
-      $stream_array = $this->SetStream("Status", $signature->contractID);
-
-      $response = file_get_contents($stream_array['url'], false, $stream_array['contextOptions']);
-
-      if ($response === false) {
-        return "Error";
-      }
-      else {
-
-        $responseData = json_decode($response, true);
-
-        if ($responseData["submitters"][0]["opened_at"] == null) {
-          $signature->created_at = strtotime($responseData["submitters"][0]["sent_at"]);
-          $signature->save();
-          return "Sent";
-        } elseif ($responseData["submitters"][0]["completed_at"] == null) {
-          $signature->status = "opened";
-          $signature->updated_at = strtotime($responseData["submitters"][0]["opened_at"]);
-          $signature->save();
-          return "Opened";
-        } else {
-          $signature->status = "signed";
-          $signature->updated_at = strtotime($responseData["submitters"][0]["completed_at"]);
-          $signature->save();
-          return "Signed";
+            if(isset($responseData["submitters"][0]["completed_at"])) {
+                $signature->updated_at = strtotime($responseData["submitters"][0]["completed_at"]);
+                $status = "signed";
+            }
         }
-      } 
-    } 
 
+        $signature->status = $status;
+        $signature->save();
+
+        return $status;
+    }
 }
-
-
-  
-
