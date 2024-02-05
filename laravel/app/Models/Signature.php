faba890097096f0808aa8dd578e862ce192ae60d
Rachel Fish <itsrachelfish@gmail.com>
2024-02-05 00:43:24 -0700

    Fixed signature model relationship

--- laravel/app/Models/Signature.php
+++ laravel/app/Models/Signature.php
@@ -6,13 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Signature extends Model
 {
- 
+
     protected $table = "signatures";
     protected $fillable = ['contractID', 'slug', 'status'];
     protected $dates = ['sent', 'opened', 'signed'];
 
     // Signature belongs to an application
-    public function user()
+    public function application()
     {
         return $this->belongsTo('App\Models\Application');
     }
